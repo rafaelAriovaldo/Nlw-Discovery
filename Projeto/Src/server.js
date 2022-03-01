45a4b535575756e45ed3117d72218321add905f2
Rafael <rafaelariovaldo02@gmail.com>
2022-03-01 01:06:43 -0300

    update-code js page refresh.

--- Projeto/Src/server.js
+++ Projeto/Src/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const server = express()
+server.listen(3000, () => console.log("Run"))
 const route = require('./route')
 const path = require('path')
+const { dirname } = require('path')
 
-const server = express()
 server.set('view engine', 'ejs')
 server.use(express.static("public"))
 server.set('views', path.join(__dirname, 'views'))
 server.use(route)
-
-server.listen(3000, () => console.log("Run"))
