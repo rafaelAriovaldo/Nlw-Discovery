6c4d1ff934273cb35c4479b2aa21454b37650c9a
Rafael <rafaelariovaldo02@gmail.com>
2022-03-04 15:17:45 -0300

    up-date modal revised.

--- Projeto/public/scripts/main.js
+++ Projeto/public/scripts/main.js
@@ -2,19 +2,26 @@ import Modal from './modal.js'
 
 const modal = Modal();
 //pegar todos os botoes da classe check
+const modalTitle = document.querySelector('.modal h2 ');
+const modalDescription = document.querySelector('.modal p ');
+const modalButton = document.querySelector('.modal button ');
+
 const checkButtons = document.querySelectorAll(".actions a.check")
     //Marcar como lido quando clicado
 checkButtons.forEach(button => {
         //adicionar evento
         button.addEventListener("click", event => {
+            modalTitle.innerHTML = "Marcar como lida"
             modal.open()
         })
     })
     //Abrir modal
     //delete clicado abre a modal
 const deleteButton = document.querySelectorAll(".actions a.delete")
+
 deleteButton.forEach(button => {
     button.addEventListener("click", event => {
+        modalTitle.innerHTML = "Excluir pergunta ?"
         modal.open()
     })
 
