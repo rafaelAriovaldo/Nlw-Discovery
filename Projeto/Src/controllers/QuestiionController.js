faecea32d83bf2b0f976123ecad29d40781cf801
Rafael <rafaelariovaldo02@gmail.com>
2022-03-09 11:30:20 -0300

    password code update.

--- Projeto/Src/controllers/QuestiionController.js
+++ Projeto/Src/controllers/QuestiionController.js
@@ -1,9 +1,12 @@
 module.exports = {
     index(req, res) {
-        const roomId = req.params.room;
-        const questionId = req.params.question;
+        const roomId = req.params.room
+        const questionId = req.params.question
         const action = req.params.action
         const password = req.body.password
+
+        console.log(`room = ${roomId}, questionId = ${questionId}, action =${action},
+        password = ${password}`)
 
     }
 }
