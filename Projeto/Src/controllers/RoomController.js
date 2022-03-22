3a9bcbc217a50ba899c2459671515adc9a4d046a
Rafael <rafaelariovaldo02@gmail.com>
2022-03-22 00:10:16 -0300

    UPDATE code Surface Exploration.

--- Projeto/Src/controllers/RoomController.js
+++ Projeto/Src/controllers/RoomController.js
@@ -5,15 +5,22 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
+        let isRoom = true
+        while (roomsEXids) {}
+        //gera o numero da sala
         for (var i = 0; i < 6; i++) {
             i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
                 roomId += Math.floor(Math.random() * 10).toString()
 
-
+            const roomsEXids = await db.all(`SELECT id FROM rooms`)
+            roomsEXids.some(roomsEXids => roomsEXids === roomId)
         }
-        await db.run(`INSERT INTO rooms(
+        if (!roomsEXids) {
+            await db.run(`INSERT INTO rooms(
             id, pass) VALUES(${parseInt(roomId)}, ${pass})`)
-        await db.close()
-        res.redirect(`/room/ ${ roomId }`)
+            await db.close()
+            res.redirect(`/room/ ${ roomId }`)
+        }
+
     }
 }
