b1232303dff6e9f23cef84e535bcca44656f7033
Rafael <rafaelariovaldo02@gmail.com>
2022-03-30 14:51:49 -0300

    update code

--- Projeto/Src/controllers/QuestiionController.js
+++ Projeto/Src/controllers/QuestiionController.js
@@ -1,15 +1,21 @@
 const Database = require('../db/config')
 module.exports = {
 
-    index(req, res) {
+    async index(req, res) {
+        const db = await Database()
         const roomId = req.params.room
         const questionId = req.params.question
         const action = req.params.action
         const password = req.body.password
-
-        console.log(`room = ${roomId}, questionId = ${questionId}, action =${action},
-        password = ${password}`)
-
+        const veryfyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`)
+        if (veryfyRoom.pass == password) {
+            if (action == "delete") {
+                await db.run(`DELETE FROM questions WHERE id = ${questionId}`)
+            } else if (action == "check") {
+                await db.run(`UPDATE questions  SET read = 1 WHERE id = ${questionId}`)
+            }
+        }
+        res.redirect(`/room/${roomId}`)
     },
     async create(req, res) {
         const db = await Database()
